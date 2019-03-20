3900e18fbb36f1f9e21ddc9f43dbdc9adef25473
Gennady Petukhov <gpetukhov@phoenixit.ru>
2019-03-20 15:57:38 +0300

    Add login button

--- app/src/main/java/com/gpetuhov/android/samplefacebook/MainActivity.kt
+++ app/src/main/java/com/gpetuhov/android/samplefacebook/MainActivity.kt
@@ -1,12 +1,51 @@
 package com.gpetuhov.android.samplefacebook
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.facebook.CallbackManager
+import com.facebook.FacebookException
+import com.facebook.login.LoginResult
+import com.facebook.FacebookCallback
+import com.pawegio.kandroid.toast
+import java.util.Arrays.asList
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val EMAIL = "email"
+    }
+
+    private var callbackManager = CallbackManager.Factory.create()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loginButton.setReadPermissions(asList(EMAIL))
+        // If you are using in a fragment, call loginButton.setFragment(this);
+
+        // Callback registration
+        loginButton.registerCallback(callbackManager, object : FacebookCallback<LoginResult> {
+            override fun onSuccess(loginResult: LoginResult) {
+                toast("Login success")
+            }
+
+            override fun onCancel() {
+                toast("Login cancel")
+            }
+
+            override fun onError(exception: FacebookException) {
+                toast("Login error")
+            }
+        })
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        // This is required to pass results to the LoginManager
+        // (and for the callback to be triggered).
+        callbackManager.onActivityResult(requestCode, resultCode, data)
+        super.onActivityResult(requestCode, resultCode, data)
     }
 }
